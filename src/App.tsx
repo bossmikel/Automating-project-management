acdeb702a78da481a40727dcac3df2c35d347b92
ESUM BRANDON <esumbrandon074@gmail.com>
2021-12-27 15:22:42 +0100

    Updates in splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Box, Typography } from '@mui/material';
+import PropagateLoader from "react-spinners/PropagateLoader";
 
-
-import SignUp from './SignUp/SignUp';
+import Home from './Home/Home';
+import SignIn from './SignIn/SignIn'
+import SignUp from './SignUp/SignUp'
 
 function App() {
-  return <SignUp />
+  const  [loading, setLoading] = useState( false ) 
+
+useEffect (()=>{
+
+  setLoading (true)
+  setTimeout (()=>{
+    setLoading(false)
+  }, 8000)
+}, [])
+
+  return (
+    <Box sx ={{
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box'
+    }}>
+      {
+        loading ?
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          backgroundColor: '#000000',
+          height: '100vh',
+          width: '100%'
+        }}>
+          <Typography variant = 'h4' color ='#ffffff'>Welcome to APM</Typography>
+          <Typography variant = 'h4' color ='#ffffff'>Manage your projects with ease!</Typography>
+        <PropagateLoader color='#1838DB' loading={loading} size={15} />
+          
+        </Box>
+        :
+        
+        <Home />
+      }
+ 
+  </Box>
+  );
 }
 
 export default App;
